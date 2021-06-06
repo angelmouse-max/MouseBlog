8d4ea54d3d1160720baf00ce390e8fc368a5ebca
angelmouse-max <81682134+angelmouse-max@users.noreply.github.com>
2021-06-06 14:27:38 +0800

    modify the error

--- MouseBlog/settings.py
+++ MouseBlog/settings.py
@@ -152,5 +152,5 @@ if cwd =='/app' or cwd[:4] =='/tmp':
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = 'staticfiles'
     STATICFILES_DIRS = (
-        os.path.join(BASE_DIR,'static')
+        os.path.join(BASE_DIR,'static'),
     )
